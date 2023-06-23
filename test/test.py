b27551aadb316dabf3f3c6979f9e26b97c49733a
pewpewpoop <jessdoitnike@gmail.com>
2023-06-23 23:52:46 +0200

    finishleanring

--- test/test.py
+++ test/test.py
@@ -1,1 +1,14 @@
-print("hello world") #this is how you comment in python? lmao, i also miss semicolons
+import numpy as np
+scan_a = input("Enter First digit: ")
+scan_b = input("Enter second digit: ")
+
+
+def myfunc(num1, num2):
+    if num1*num2 <= 1000:
+        return num1 * num2
+    else:
+        return num1 + num2
+    
+my_string = "The result is {}"
+print(my_string.format(myfunc(int(scan_a), int(scan_b))))
+
